5b74183cf4866771f41e8550a37fc07823092ae0
Ismagilov Marat <95739983+Marat1242@users.noreply.github.com>
2022-12-27 16:49:16 +0300

    Add files via upload

---
+++ Week4_Backend/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ORIS.week5
+{
+    public class HttpServer : IDisposable
+    {
+        private readonly HttpListener listener;
+
+        public HttpServer()
+        {
+            listener = new HttpListener();
+            listener.Prefixes.Add($"http://localhost:8888/google/");
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Запуск сервера");
+            listener.Start();
+            Console.WriteLine("Сервер запущен");
+            Receive();
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Остановка сервера");
+            listener.Stop();
+            Console.WriteLine("Сервер остановлен");
+        }
+
+        private void Receive()
+        {
+            listener.BeginGetContext(new AsyncCallback(ListenerCallback), listener);
+        }
+
+        private void ListenerCallback(IAsyncResult result)
+        {
+            if (listener.IsListening)
+            {
+                var context = listener.EndGetContext(result);
+                var request = context.Request;
+
+                // получаем объект ответа
+                var response = context.Response;
+
+                // создаем ответ в виде кода html
+                string responseStr = File.ReadAllText("/Users/alsufaizova/Projects/ORIS/ORIS/week5/index.html");
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseStr);
+
+                // получаем поток ответа и пишем в него ответ
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+
+                // закрываем поток
+                output.Close();
+
+                Receive();
+            }
+        }
+
+        public void Dispose()
+        {
+            listener.Stop();
+        }
+    }
+}
